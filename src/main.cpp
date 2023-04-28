f0a05f48aec3385a0158a27b05aadba0b7b24016
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2023-04-28 16:19:22 +0200

    main file updated to right motor library, first working commit. Now code will be updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,63 @@
-/*-------------------------------------------------------------------------------
-Created on Sat Feb 27 2021
-
-@Author: Jihwan Lee
-@GIT ID: DGIRobo
-@E-mail: fist5678@dgist.ac.kr
-@Department: Daegu Gyeongbuk Institute of Science and Technology(DGIST) Undergraduate Course
--------------------------------------------------------------------------------*/
-#include <Arduino.h>
 #include <mcp_can.h>
+
 #include <SPI.h>
-#include "gim8008V3.h"
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-#ifdef ARDUINO_SMD_VARIANT_COMPLANCE
-#define SERIAL SerialUSB
-#else
-#define SERIAL Serial
-#endif
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-#define UP A1
-#define DOWN A3
-#define LEFT A2
-#define RIGHT A5
-#define CLICK A4
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-#define LED2 8
-#define LED3 7
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// VARIABLES PARA CONTROLAR EL TIEMPO
-unsigned long previousMillis = 0;
-const long interval = 20;
-unsigned long arduinoLoopTime;
-unsigned long previousLooptime;
-double t;
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-gim8008V3 motor(1, 205, 5);
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-float actPos0 = 0;
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-const int SPI_CS_PIN = 10;
-MCP_CAN CAN(SPI_CS_PIN);
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-void setup()
-{
-  Serial.begin(9600);
-  delay(1000);
-  while (CAN_OK != CAN.begin(1))
+
+//Serial Peripherial Interface (SPI) Library
+
+#include <gim8115.h> //Include object
+
+#define CAN_INT 2 //CAN BUS interrupt pin (NOT USED)
+
+#define spiCSPin 10 //The MCP2515 CAN Bus is connected to pin 10
+
+float frequence = 0.01;
+
+float amplitude = 3.14;
+
+volatile float pos = 0;
+
+float angular_velocity = 1;
+
+float pos_pertubration = 0;
+
+float time_2 = 1 ;
+
+
+int pin_interrupt = 2; //Pin 2 is the interrupt
+
+volatile int state = LOW; // Giving the 
+
+//CAN BUS initialization  
+
+MCP_CAN CAN(spiCSPin);  //Setting the CS Pin number
+
+//Motor initialization
+gim8115 motor(0x01, 50, 0.5);
+
+
+void setup() {
+  Serial.begin(9600); //Begin Serial port
+
+  while(CAN_OK != CAN.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ))  //Begin the CAN Bus and set frequency to 8 MHz and baudrate of 1000kb/s  and the masks and filters disabled.
   {
-    SERIAL.println("CAN BUS Shield init fail");
-    // SERIAL.println("Init CAN BUST Shield again");
-    delay(2000);
-  }
-  Serial.println("CAN BUS Shield init ok!");
+    Serial.println("CAN Bus initialization failed"); 
+    Serial.println("Initializing CAN Bus again");
+    delay(100);
+    }
+    CAN.setMode(MCP_NORMAL);  // Change to normal mode to allow messages to be transmitted and received
+   Serial.println("CAN Bus init OK");
+   motor.exitMotormode(CAN);
+   motor.setMotormode(CAN);
+   motor.setZero(CAN);
 
-  pinMode(UP, INPUT);
-  pinMode(DOWN, INPUT);
-  pinMode(LEFT, INPUT);
-  pinMode(RIGHT, INPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
 
-  digitalWrite(UP, HIGH);
-  digitalWrite(DOWN, HIGH);
-  digitalWrite(LEFT, HIGH);
-  digitalWrite(RIGHT, HIGH);
-
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
+  //pinMode(CAN_INT, INPUT);  //Configuring the interrupt pin from the MCP(NOT USED)
 }
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-void loop()
-{
-  if (Serial.available())
+
+void loop() {
+//float pos = 1;
+if (Serial.available())
   { // Enquanto a Serial receber dados
     delay(10);
     String comando = "";
@@ -83,22 +68,25 @@ void loop()
     }
     if (comando == "a")
     {
-      actPos0 = motor.normalSet(CAN, 2, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
-      Serial.println(actPos0);
+      motor.normalSet(CAN, 2, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
+      // Serial.println(actPos0);
     }
     if (comando == "s")
     {
-      actPos0 = motor.normalSet(CAN, 10, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
-      Serial.println(actPos0);
+      motor.normalSet(CAN, 10, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
+      // Serial.println(actPos0);
     }
     if (comando == "f")
     {
-      actPos0 = motor.normalSet(CAN, 1000, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
-      Serial.println(actPos0);
+      motor.normalSet(CAN, 1000, 2, 2); // Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
+      // Serial.println(actPos0);
     }
     if (comando == "d")
     {
       Serial.println("oi");
     }
   }
+  motor.
+// motor.normalSet(CAN, pos, 0, 0); //Put in order: CANObject, desirePosition(rad), maxVelocity(rad/s), maxTorque(rad/s^2)
+
 }
