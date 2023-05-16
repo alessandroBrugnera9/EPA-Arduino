f7d54dd1ca0799677fb513c637abcd1710074315
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2023-05-16 22:14:34 +0200

    Creating individual functions for handling the motor parameters

--- lib/gim8115/gim8115.cpp
+++ lib/gim8115/gim8115.cpp
@@ -1,8 +1,8 @@
 #include "gim8115.h"
-#include <SPI.h>  //Serial Peripherial Interface (SPI) Library
+#include <SPI.h> //Serial Peripherial Interface (SPI) Library
 
-
-int gim8115::setMotormode(MCP_CAN& CAN){
+int gim8115::setMotormode(MCP_CAN &CAN)
+{
   unsigned char len = 0;
   unsigned char buf[8];
   buf[0] = 0xFF;
@@ -14,17 +14,18 @@ int gim8115::setMotormode(MCP_CAN& CAN){
   buf[6] = 0xFF;
   buf[7] = 0xFC;
 
-  while(CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
-{
- //Serial.println("Entering Motor Mode Failed!");
- //Serial.println("Initializing Motor again");
- delay(100);
-}
-//Serial.println("Motor Mode Enabled!");
+  while (CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
+  {
+    // Serial.println("Entering Motor Mode Failed!");
+    // Serial.println("Initializing Motor again");
+    delay(100);
+  }
+  // Serial.println("Motor Mode Enabled!");
   return 1;
 }
 
-int gim8115::exitMotormode(MCP_CAN& CAN){
+int gim8115::exitMotormode(MCP_CAN &CAN)
+{
   unsigned char len = 0;
   unsigned char buf[8];
   buf[0] = 0xFF;
@@ -35,18 +36,19 @@ int gim8115::exitMotormode(MCP_CAN& CAN){
   buf[5] = 0xFF;
   buf[6] = 0xFF;
   buf[7] = 0xFD;
-    
-    while(CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
-{
- //Serial.println("Exiting Motor Mode Failed!");
- //Serial.println("Exiting Motor again");
- delay(100);
-}
-//Serial.println("Motor Mode Disabled");
+
+  while (CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
+  {
+    // Serial.println("Exiting Motor Mode Failed!");
+    // Serial.println("Exiting Motor again");
+    delay(100);
+  }
+  // Serial.println("Motor Mode Disabled");
   return 1;
 }
 
-int gim8115::setZero(MCP_CAN& CAN){
+int gim8115::setZero(MCP_CAN &CAN)
+{
   unsigned char len = 0;
   unsigned char buf[8];
   buf[0] = 0xFF;
@@ -57,120 +59,149 @@ int gim8115::setZero(MCP_CAN& CAN){
   buf[5] = 0xFF;
   buf[6] = 0xFF;
   buf[7] = 0xFE;
-    
-    while(CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
-{
- //Serial.println("Zero Setting Failed!");
- //Serial.println("Zero Setting again");
- delay(100);
-}
-//Serial.println("Zero Setted");
+
+  while (CAN_OK != CAN.sendMsgBuf(id, 0, 8, buf))
+  {
+    // Serial.println("Zero Setting Failed!");
+    // Serial.println("Zero Setting again");
+    delay(100);
+  }
+  // Serial.println("Zero Setted");
   return 1;
 }
 
+float gim8115::normalSet(MCP_CAN &CAN, float tarPos, float tarVel, float tarTor)
+{
+  // Call the private functions to build the individual packages
+  float *posPackage = buildPositionPackage(tarPos);
+  float *velPackage = buildVelocityPackage(tarVel);
+  float *torPackage = buildTorquePackage(tarTor);
 
-float gim8115::normalSet(MCP_CAN& CAN, float tarPos, float tarVel, float tarTor){
-//Position//
+  
+  //----------------------------------------------------------------------------//
+  // Sending data//
+  unsigned char len = 0;
+  unsigned char buf[8];
+  buf[0] = posPackage[0];
+  buf[1] = posPackage[1];
+  buf[2] = velPackage[0];
+  buf[3] = velPackage[1] * 16 + kp_16h_hex;
+  buf[4] = kp_16l_hex;
+  buf[5] = kd_16h_hex;
+  buf[6] = kd_16l_hex * 16 + torPackage[0];
+  buf[7] = torPackage[1];
+
+  byte sndStat = CAN.sendMsgBuf(id, 0, 8, buf);
+
+  if (sndStat == CAN_OK)
+  {
+    // Serial.println("Message Sent Successfully!");
+  }
+  else
+  {
+    // Serial.println("Error Sending Message!");
+  }
+
+  //----------------------------------------------------------------------------//
+  // Receiving data//
+  long unsigned int rxId;
+  char msgString2[128]; // Array to store serial string
+  byte buf_received[6];
+  char print_pos[15]; // Position string
+  char print_vel[15]; // Velocity string
+  char print_cur[15]; // Current string
+
+  CAN.readMsgBuf(&rxId, &len, buf_received); // CAN BUS reading
+
+  // Collecting bits from CAN_BUS signal
+  unsigned int pos_motor = ((buf_received[1] << 8) | buf_received[2]);        // Position reading
+  unsigned int vel_motor = (buf_received[3] << 4) | ((buf_received[4]) >> 4); // Velocity reading
+  unsigned int cur_motor = ((buf_received[4] & 0xF) << 8) | buf_received[5];  // Current reading
+
+  // Converting to readable data//
+
+  float pos_f = (float)pos_motor;
+  pos_f = (pos_f * 191 / 65535) - 95.5;
+  float vel_f = (float)vel_motor;
+  vel_f = (vel_f * 90 / 4095) - 45;
+  float cur_f = (float)cur_motor;
+  cur_f = (cur_f * 36 / 4095) - 18;
+
+  // Tranform to string to be printable with sprintf
+  dtostrf(pos_f, 6, 3, print_pos);
+  dtostrf(vel_f, 6, 3, print_vel);
+  dtostrf(cur_f, 6, 3, print_cur);
+
+  sprintf(msgString2, "Motor Id: %d  Position: %s  Velocity: %s  Current: %s", buf_received[0], print_pos, print_vel, print_cur);
+  Serial.println(msgString2);
+  // Serial.println("desired_Position: ");
+  // Serial.write((unsigned int) pos_f);
+
+  return 1;
+}
+
+float *gim8115::buildPositionPackage(float tarPos)
+{
+  float *posPackage = new float[2];
+
+  // Position package building code
   unsigned int pos_b;
-  if (tarPos>pos_max)
+  if (tarPos > pos_max)
   {
     tarPos = pos_max;
   }
-  else if (tarPos<pos_min)
+  else if (tarPos < pos_min)
   {
     tarPos = pos_min;
   }
   tarPos = tarPos + 95.5;
   pos_b = 65535 * tarPos / 191;
-  unsigned char pos_16h = pos_b / 256;
-  unsigned char pos_16l = pos_b % 256;
+  posPackage[0] = pos_b / 256;
+  posPackage[1] = pos_b % 256;
 
-  //----------------------------------------------------------------------------//
-  //Velocity//
+  return posPackage;
+}
+
+float *gim8115::buildVelocityPackage(float tarVel)
+{
+  float *velPackage = new float[2];
+
+  // Velocity package building code
   unsigned int vel_b;
-  if (tarVel>vel_max)
+  if (tarVel > vel_max)
   {
     tarVel = vel_max;
   }
-  else if (tarVel<vel_min)
+  else if (tarVel < vel_min)
   {
     tarVel = vel_min;
   }
   tarVel = tarVel + 45;
   vel_b = 4095 * tarVel / 90;
-  unsigned char vel_16h = vel_b / 16;
-  unsigned char vel_16l = vel_b % 16;
-  //----------------------------------------------------------------------------//
-  //Torque//
+  velPackage[0] = vel_b / 16;
+  velPackage[1] = vel_b % 16;
+
+  return velPackage;
+}
+
+float *gim8115::buildTorquePackage(float tarTor)
+{
+  float *torPackage = new float[2];
+
+  // Torque package building code
   unsigned int tor_b;
-  if (tarTor>tor_max)
+  if (tarTor > tor_max)
   {
     tarTor = tor_max;
   }
-  else if (tarTor<tor_min)
+  else if (tarTor < tor_min)
   {
     tarTor = tor_min;
   }
   tarTor = tarTor + 18;
   tor_b = 4095 * tarTor / 36;
-  unsigned char tor_16h = tor_b / 256;
-  unsigned char tor_16l = tor_b % 256;
-  //----------------------------------------------------------------------------//
-  //Sending data//
-  unsigned char len = 0;
-  unsigned char buf[8];
-  buf[0] = pos_16h;
-  buf[1] = pos_16l;
-  buf[2] = vel_16h;
-  buf[3] = vel_16l*16 + kp_16h_hex;
-  buf[4] = kp_16l_hex;
-  buf[5] = kd_16h_hex;
-  buf[6] = kd_16l_hex*16 + tor_16h;
-  buf[7] = tor_16l;
+  torPackage[0] = tor_b / 256;
+  torPackage[1] = tor_b % 256;
 
-  byte sndStat =  CAN.sendMsgBuf(id, 0, 8, buf);
-
-  if(sndStat == CAN_OK){
-	//Serial.println("Message Sent Successfully!");
-  } else{
-	//Serial.println("Error Sending Message!");
-	}
-
-  //----------------------------------------------------------------------------//
-  //Receiving data//
-  long unsigned int rxId;
-  char msgString2[128];  // Array to store serial string
-  byte buf_received[6];
-  char print_pos[15];   //Position string
-  char print_vel[15];   //Velocity string
-  char print_cur[15];   //Current string
-
-
-    CAN.readMsgBuf(&rxId ,&len, buf_received);  //CAN BUS reading
-
-    //Collecting bits from CAN_BUS signal
-   unsigned int pos_motor = ((buf_received[1]<<8)|buf_received[2]); //Position reading
-   unsigned int vel_motor = (buf_received[3]<<4)|((buf_received[4])>>4);    //Velocity reading
-   unsigned int cur_motor = ((buf_received[4]&0xF)<<8)|buf_received[5]; //Current reading
-
-   //Converting to readable data//
-
-   float pos_f = (float) pos_motor;
-   pos_f = (pos_f*191/65535)-95.5;
-   float vel_f = (float) vel_motor;
-   vel_f = (vel_f*90/4095)-45;
-   float cur_f = (float) cur_motor;
-   cur_f = (cur_f*36/4095)-18;      
-
-    //Tranform to string to be printable with sprintf
-   dtostrf(pos_f, 6, 3, print_pos);
-   dtostrf(vel_f, 6, 3, print_vel);
-   dtostrf(cur_f, 6, 3, print_cur);
-
-  sprintf(msgString2,"Motor Id: %d  Position: %s  Velocity: %s  Current: %s", buf_received[0],  print_pos, print_vel, print_cur);
-  Serial.println(msgString2);
-  //Serial.println("desired_Position: ");
-  //Serial.write((unsigned int) pos_f);
-  
-  return 1;
+  return torPackage;
 }
--- lib/gim8115/gim8115.h
+++ lib/gim8115/gim8115.h
@@ -1,76 +1,111 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-
 #define pi = 3.14
 
 class gim8115
 {
-  private:
-    unsigned char id;
-    //----------------------------------------//
-    float kp_max = 500;
-    float kp_min = 0;
-    unsigned int kp_b;
-    float kp_16h;
-    float kp_16l;
-    unsigned char kp_16h_hex;
-    unsigned char kp_16l_hex;
-    //----------------------------------------//
-    float kd_max = 5;
-    float kd_min = 0;
-    unsigned int kd_b;
-    float kd_16h;
-    float kd_16l;
-    unsigned char kd_16h_hex;
-    unsigned char kd_16l_hex;
-    //----------------------------------------//
-    float pos_max = 6.28*20;
-    float pos_min = -6.28*20;
-    //----------------------------------------//
-    float vel_max = 45;
-    float vel_min = -45;
-    //----------------------------------------//
-    float tor_max = 18;
-    float tor_min = -18;
-    
-  public:
-    gim8115(int _id, float _kp, float _kd){
-      id = (unsigned char) _id;
-      //----------------------------------------//
-      if (_kp>kp_max)
-      {
-        _kp = kp_max;
-      }
-      else if (_kp<kp_min)
-      {
-        _kp = kp_min;
-      }
-      kp_b = 4095 * _kp / 500;
-      kp_16h = kp_b / 256;
-      kp_16l = kp_b % 256;
-      kp_16h_hex = kp_16h;
-      kp_16l_hex = kp_16l;
-      //----------------------------------------//
-      if (_kd>kd_max)
-      {
-        _kd = kd_max;
-      }
-      else if (_kd<kd_min)
-      {
-        _kd = kd_min;
-      }
-      kd_b = 4095 * _kd / 5;
-      kd_16h = kd_b / 16;
-      kd_16l = kd_b % 16;
-      kd_16h_hex = kd_16h;
-      kd_16l_hex = kd_16l;
+private:
+  unsigned char id;
+  //----------------------------------------//
+  float kp_max = 500;
+  float kp_min = 0;
+  unsigned int kp_b;
+  float kp_16h;
+  float kp_16l;
+  unsigned char kp_16h_hex;
+  unsigned char kp_16l_hex;
+  //----------------------------------------//
+  float kd_max = 5;
+  float kd_min = 0;
+  unsigned int kd_b;
+  float kd_16h;
+  float kd_16l;
+  unsigned char kd_16h_hex;
+  unsigned char kd_16l_hex;
+  //----------------------------------------//
+  float pos_max = 6.28 * 20;
+  float pos_min = -6.28 * 20;
+  //----------------------------------------//
+  float vel_max = 45;
+  float vel_min = -45;
+  //----------------------------------------//
+  float tor_max = 18;
+  float tor_min = -18;
 
+  /**
+   * @brief Builds the position package for the GIM8115 device.
+   *
+   * This function takes the target position and returns an array of size 2 representing the position package.
+   * @param tarPos The target position.
+   * @return An array of size 2 representing the position package.
+   */
+  float *buildPositionPackage(float tarPos);
+
+  /**
+   * @brief Builds the velocity package for the GIM8115 device.
+   *
+   * This function takes the target velocity and returns an array of size 2 representing the velocity package.
+   * @param tarVel The target velocity.
+   * @return An array of size 2 representing the velocity package.
+   */
+  float *buildVelocityPackage(float tarVel);
+
+  /**
+   * @brief Builds the torque package for the GIM8115 device.
+   *
+   * This function takes the target torque and returns an array of size 2 representing the torque package.
+   * @param tarTor The target torque.
+   * @return An array of size 2 representing the torque package.
+   */
+  float *buildTorquePackage(float tarTor);
+
+public:
+  gim8115(int _id, float _kp, float _kd)
+  {
+    id = (unsigned char)_id;
+    //----------------------------------------//
+    if (_kp > kp_max)
+    {
+      _kp = kp_max;
     }
+    else if (_kp < kp_min)
+    {
+      _kp = kp_min;
+    }
+    kp_b = 4095 * _kp / 500;
+    kp_16h = kp_b / 256;
+    kp_16l = kp_b % 256;
+    kp_16h_hex = kp_16h;
+    kp_16l_hex = kp_16l;
+    //----------------------------------------//
+    if (_kd > kd_max)
+    {
+      _kd = kd_max;
+    }
+    else if (_kd < kd_min)
+    {
+      _kd = kd_min;
+    }
+    kd_b = 4095 * _kd / 5;
+    kd_16h = kd_b / 16;
+    kd_16l = kd_b % 16;
+    kd_16h_hex = kd_16h;
+    kd_16l_hex = kd_16l;
+  }
 
-    int setMotormode(MCP_CAN& CAN);
-    int exitMotormode(MCP_CAN& CAN);
-    int setZero(MCP_CAN& CAN);
-    float normalSet(MCP_CAN& CAN, float tarPos, float tarVel, float tarTor);
+  int setMotormode(MCP_CAN &CAN);
+  int exitMotormode(MCP_CAN &CAN);
+  int setZero(MCP_CAN &CAN);
 
+  /**
+   * @brief Sets the normal mode for the GIM8115 device.
+   *
+   * This function sets the position, velocity, and torque for the GIM8115 device and sends the command package.
+   * @param tarPos The target position.
+   * @param tarVel The target velocity.
+   * @param tarTor The target torque.
+   * @return 1 if the message was sent successfully, 0 otherwise.
+   */
+  float normalSet(MCP_CAN &CAN, float tarPos, float tarVel, float tarTor);
 };
