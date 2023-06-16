b0affc56db89f74e37c1e6bbfea9d8caec99f7b1
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2023-06-16 11:45:21 +0200

    optimizing code

--- src/MotorHandler.cpp
+++ src/MotorHandler.cpp
@@ -29,12 +29,7 @@ void MotorHandler::setTorqueMode(float tarTor)
   //----------------------------------------------------------------------------//
   // Sending data//
   unsigned char buf[8];
-  buf[0] = 0;
-  buf[1] = 0;
-  buf[2] = 0;
-  buf[3] = 0;
-  buf[4] = 0;
-  buf[5] = 0;
+  memset(buf, 0, sizeof(buf)); //setting array to 0
   buf[6] = torPackage[0];
   buf[7] = torPackage[1];
 
@@ -63,16 +58,16 @@ void MotorHandler::setBaseTorque(float trq)
 
 void MotorHandler::printPrettyResponse(motorResponse res)
 {
-  char msgString2[64]; // Array to store serial string
-  char print_pos[10];  // Position string
-  char print_vel[10];  // Velocity string
-  char print_cur[10];  // Current string
+  char msgString2[64];     // Array to store serial string
+  char posString[10];      // Position string
+  char velocityString[10]; // Velocity string
+  char currentString[10];  // Current string
 
   // Transform to string to be printable with snprintf
-  snprintf(print_pos, sizeof(print_pos), "%.3f", res.position);
-  snprintf(print_vel, sizeof(print_vel), "%.3f", res.velocity);
-  snprintf(print_cur, sizeof(print_cur), "%.3f", res.current);
+  snprintf(posString, sizeof(posString), "%.3f", res.position);
+  snprintf(velocityString, sizeof(velocityString), "%.3f", res.velocity);
+  snprintf(currentString, sizeof(currentString), "%.3f", res.current);
 
-  snprintf(msgString2, sizeof(msgString2), "Position: %s  Velocity: %s  Current: %s", print_pos, print_vel, print_cur);
+  snprintf(msgString2, sizeof(msgString2), "Position: %s  Velocity: %s  Current: %s", posString, velocityString, currentString);
   Serial.println(msgString2);
 }
